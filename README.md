14997c2713016a3ce448c21bb4b450e1a1884a12
dh-r-uv <Dhruv.Kothari@iitb.ac.in>
2023-12-03 13:53:20 +0530

    added script to maintain count of solved problems

--- README.md
+++ README.md
@@ -1,7 +1,39 @@
 # CSES
+# Competitive Programming Repository
 
-`Just me trying to post my solutions to the CSES peoblem set, all my solutions are in cpp`
+This repository contains solutions to various competitive programming problems. The solutions are categorized into different folders based on topics.
 
-`Have completed all the problems under the Introductory Problems`
+## Folder Structure
 
-`Still have a long way to go tho, will keep updating as I solve all the problems`
+- **Dynamic Programming:9** *<number of files>*
+- **Geometry:0** *<number of files>*
+- **Introductory Problems:19** *<number of files>*
+- **Maths:0** *<number of files>*
+- **Range Queries:0** *<number of files>*
+- **Searching and Sorting:0** *<number of files>*
+- **Trees:3** *<number of files>*
+- **Strings:0** *<number of files>*
+
+## Total Files: 31*<sum of all files>*
+
+---
+
+## Usage
+
+### Update File Counts
+
+To automatically update the counts of files in each folder and the total count, you can use the provided script. The script utilizes the `find` command to count the files in each folder and updates the README file accordingly.
+
+#### Prerequisites
+
+- Unix-like environment (Linux or macOS)
+- Bash shell
+
+#### How to Use
+
+1. Make the script executable:
+
+   ```bash
+   chmod +x update_readme.sh
+- 2023-12-03 13:49: Auto-update counts.
+- 2023-12-03 13:50: Auto-update counts.
---
+++ update_readme.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Function to count files in a folder
+count_files() {
+  folder="$1"
+  count=$(find "$folder" -type f | wc -l)
+  echo "$count"
+}
+
+# Update Dynamic Programming count
+dp_count=$(count_files "Dynamic_Programming")
+sed -i "s/\(Dynamic Programming: *\)[0-9]*/\1$dp_count/" README.md
+
+# Update Geometry count
+geometry_count=$(count_files "Geometry")
+sed -i "s/\(Geometry: *\)[0-9]*/\1$geometry_count/" README.md
+
+# Update Introductory Problems count
+intro_problems_count=$(count_files "Introductory_problems")
+sed -i "s/\(Introductory Problems: *\)[0-9]*/\1$intro_problems_count/" README.md
+
+# Update Maths count
+maths_count=$(count_files "Maths")
+sed -i "s/\(Maths: *\)[0-9]*/\1$maths_count/" README.md
+
+# Update Range Queries count
+range_queries_count=$(count_files "Range_Queries")
+sed -i "s/\(Range Queries: *\)[0-9]*/\1$range_queries_count/" README.md
+
+# Update Searching and Sorting count
+search_sort_count=$(count_files "Searching_and_Sorting")
+sed -i "s/\(Searching and Sorting: *\)[0-9]*/\1$search_sort_count/" README.md
+
+# Update Trees count
+trees_count=$(count_files "Trees")
+sed -i "s/\(Trees: *\)[0-9]*/\1$trees_count/" README.md
+
+# Update Strings count
+strings_count=$(count_files "Strings")
+sed -i "s/\(Strings: *\)[0-9]*/\1$strings_count/" README.md
+
+# Update Total Files count
+total_count=$((dp_count + geometry_count + intro_problems_count + maths_count + range_queries_count + search_sort_count + trees_count + strings_count))
+sed -i "s/\(Total Files: *\)[0-9]*/\1$total_count/" README.md
+
+# Add a new entry to the Update History
+current_date_time=$(date +"%Y-%m-%d %H:%M")
+echo "- $current_date_time: Auto-update counts." >> README.md
+
+echo "README.md updated successfully."
