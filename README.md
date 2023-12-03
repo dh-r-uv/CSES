d68f58fa9ffe6285ae4d165f1aa87e80c60dfa9b
dh-r-uv <Dhruv.Kothari@iitb.ac.in>
2023-12-03 14:53:25 +0530

    updates

--- README.md
+++ README.md
@@ -4,16 +4,16 @@ This repository contains my solutions to the CSES problem set
 
 ## Problems solved
 
-- **Dynamic Programming:9** *<number of files>*
-- **Geometry:1** *<number of files>*
-- **Introductory Problems:19** *<number of files>*
-- **Maths:0** *<number of files>*
-- **Range Queries:0** *<number of files>*
-- **Searching and Sorting:0** *<number of files>*
-- **Trees:3** *<number of files>*
-- **Strings:0** *<number of files>*
+- **Dynamic Programming : 9** *<number of files>*
+- **Geometry : 1** *<number of files>*
+- **Introductory Problems : 19** *<number of files>*
+- **Maths : 0** *<number of files>*
+- **Range Queries : 0** *<number of files>*
+- **Searching and Sorting : 0** *<number of files>*
+- **Trees : 3** *<number of files>*
+- **Strings : 0** *<number of files>*
 
-- **Total Problems solved: 32** *<sum of all files>*
+- **Total Problems solved : 32** *<sum of all files>*
 
 ---
 
@@ -39,3 +39,5 @@ To automatically update the counts of files in each folder and the total count, 
 - 2023-12-03 14:32: Auto-update counts.
 - 2023-12-03 14:49: Auto-update counts.
 - 2023-12-03 14:50: Auto-update counts.
+- 2023-12-03 14:52: Auto-update counts.
+- 2023-12-03 14:53: Auto-update counts.
--- update_readme.sh
+++ update_readme.sh
@@ -9,39 +9,39 @@ count_files() {
 
 # Update Dynamic Programming count
 dp_count=$(count_files "Dynamic_Programming")
-sed -i "s/\(Dynamic Programming: *\)[0-9]*/\1$dp_count/" README.md
+sed -i "s/\(Dynamic Programming :  *\)[0-9]*/\1$dp_count/" README.md
 
 # Update Geometry count
 geometry_count=$(count_files "Geometry")
-sed -i "s/\(Geometry: *\)[0-9]*/\1$geometry_count/" README.md
+sed -i "s/\(Geometry :  *\)[0-9]*/\1$geometry_count/" README.md
 
 # Update Introductory Problems count
 intro_problems_count=$(count_files "Introductory_problems")
-sed -i "s/\(Introductory Problems: *\)[0-9]*/\1$intro_problems_count/" README.md
+sed -i "s/\(Introductory Problems :  *\)[0-9]*/\1$intro_problems_count/" README.md
 
 # Update Maths count
 maths_count=$(count_files "Maths")
-sed -i "s/\(Maths: *\)[0-9]*/\1$maths_count/" README.md
+sed -i "s/\(Maths :  *\)[0-9]*/\1$maths_count/" README.md
 
 # Update Range Queries count
 range_queries_count=$(count_files "Range_Queries")
-sed -i "s/\(Range Queries: *\)[0-9]*/\1$range_queries_count/" README.md
+sed -i "s/\(Range Queries :  *\)[0-9]*/\1$range_queries_count/" README.md
 
 # Update Searching and Sorting count
 search_sort_count=$(count_files "Searching_and_Sorting")
-sed -i "s/\(Searching and Sorting: *\)[0-9]*/\1$search_sort_count/" README.md
+sed -i "s/\(Searching and Sorting :  *\)[0-9]*/\1$search_sort_count/" README.md
 
 # Update Trees count
 trees_count=$(count_files "Trees")
-sed -i "s/\(Trees: *\)[0-9]*/\1$trees_count/" README.md
+sed -i "s/\(Trees :  *\)[0-9]*/\1$trees_count/" README.md
 
 # Update Strings count
 strings_count=$(count_files "Strings")
-sed -i "s/\(Strings: *\)[0-9]*/\1$strings_count/" README.md
+sed -i "s/\(Strings :  *\)[0-9]*/\1$strings_count/" README.md
 
 # Update Total Files count
 total_count=$((dp_count + geometry_count + intro_problems_count + maths_count + range_queries_count + search_sort_count + trees_count + strings_count))
-sed -i "s/\(Total Problems solved: *\)[0-9]*/\1$total_count/" README.md
+sed -i "s/\(Total Problems solved :  *\)[0-9]*/\1$total_count/" README.md
 
 # Add a new entry to the Update History
 current_date_time=$(date +"%Y-%m-%d %H:%M")
